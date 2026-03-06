66ea07962f1efbf09a680139c5be613563872ba9
quepasalady <ladif2pnl@gmail.com>
2026-03-06 02:26:50 +0100

    Update route.ts

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
