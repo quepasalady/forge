fa0a174af5e4f37730b7e7dd19fe7796b707373f
quepasalady <ladif2pnl@gmail.com>
2026-03-06 03:12:06 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -72,8 +72,7 @@ export default function DashboardPage() {
 
   // Initialize Supabase
   const supabase = createClientComponentClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
   )
 
   useEffect(() => {
